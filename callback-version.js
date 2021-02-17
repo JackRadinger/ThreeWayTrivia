12296ab3f8fb55ead9d4b4d4787ca5b392910e67
Danny Shoun <danielshoun@pm.me>
2021-02-17 17:29:30 -0500

    Working on callbacks.

---
+++ callback-version.js
@@ -0,0 +1,8 @@
+export function getClue(callback) {
+    let request = new XMLHttpRequest()
+    request.addEventListener("readystatechange", event => {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            
+        }
+    })
+}
--- game.js
+++ game.js
@@ -1,25 +1,41 @@
 import {getClue as getClueFromPromise} from './promise-version.js'
+import {getClue as getClueFromAsyncFunction} from './async-await-version.js'
 
 window.addEventListener('DOMContentLoaded', event => {
     document
         .getElementById('use-promise')
         .addEventListener('click', event => {
             getClueFromPromise().then(clue => {
-                document
-                    .getElementById('question').innerHTML = clue.question
-                document
-                    .getElementById('answer').innerHTML = clue.answer
-                document
-                    .getElementById('value').innerHTML = clue.value
-                document
-                    .getElementById('category-title').innerHTML = clue.category.title
-                if(clue.invalid_count > 0){
-                    document
-                        .getElementByIdZ('invalid-count').innerHTML = 'invalid'
-                } else {
-                    document
-                        .getElementByIdZ('invalid-count').innerHTML = 'valid'
-                }
+                setClueElements(clue)
             }).catch(error => console.log(error))
         })
+
+    document
+        .getElementById('use-async-await')
+        .addEventListener("click", async (event) => {
+            try {
+                let clue = await getClueFromAsyncFunction()
+                setClueElements(clue)
+            } catch (e) {
+
+            }
+        })
 })
+
+function setClueElements(clue) {
+    document
+        .getElementById('question').innerHTML = clue.question
+    document
+        .getElementById('answer').innerHTML = clue.answer
+    document
+        .getElementById('value').innerHTML = clue.value
+    document
+        .getElementById('category-title').innerHTML = clue.category.title
+    if(clue.invalid_count > 0){
+        document
+            .getElementById('invalid-count').innerHTML = 'invalid'
+    } else {
+        document
+            .getElementById('invalid-count').innerHTML = 'valid'
+    }
+}
