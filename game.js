7d506c828fa09e0562b4ae455b179be1a2f392e0
Danny Shoun <danielshoun@pm.me>
2021-02-17 18:18:46 -0500

    Working on bonus.

--- game.js
+++ game.js
@@ -2,7 +2,10 @@ import {getClue as getClueFromPromise} from './promise-version.js'
 import {getClue as getClueFromAsyncFunction} from './async-await-version.js'
 import {getClue as getClueFromCallback} from './callback-version.js'
 
+let score = 0
+
 window.addEventListener('DOMContentLoaded', event => {
+    
     document
         .getElementById('use-promise')
         .addEventListener('click', event => {
@@ -32,11 +35,25 @@ window.addEventListener('DOMContentLoaded', event => {
                 }
             })
         })
+
+    document
+        .getElementById("check-response")
+        .addEventListener('click', event => {
+            let playerAnswer = document.getElementById("player-response").value
+            let answer = document.getElementById("answer")
+            if(playerAnswer.trim() === answer.innerHTML.trim()) {
+                score += Number.parseInt(document.getElementById("value").innerHTML)
+                document.getElementById("score").innerHTML = `${score}`
+                answer.classList.remove("is-hidden")
+                event.target.classList.add("is-hidden")
+            }
+        })
 })
 
 
 
 function setClueElements(clue) {
+    console.log(clue.answer)
     document
         .getElementById('question').innerHTML = clue.question
     document
@@ -52,4 +69,7 @@ function setClueElements(clue) {
         document
             .getElementById('invalid-count').innerHTML = 'valid'
     }
+    document.getElementById("check-response").classList.remove("is-hidden")
+    document.getElementById("player-response").value = ""
+    document.getElementById("answer").classList.add("is-hidden")
 }
